0d6441a39aea509ba9cad1b4d260aa6f7b904e30
Justin-Fisher495 <51326884+Justin-Fisher495@users.noreply.github.com>
2019-12-20 15:50:02 -0500

    Charting
    Added Charts to the dashboard

---
+++ JF-BugTracker/Controllers/GraphingController.cs
@@ -0,0 +1,112 @@
+﻿using JF_BugTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JF_BugTracker.Controllers
+{
+    public class GraphingController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public class MorrisBarData
+        {
+            public string label { get; set; }
+            public int value { get; set; }
+        }
+
+        public class MorrisDonutData
+        {
+            public string label { get; set; }
+            public int value { get; set; }
+        }
+
+        // GET: Graphing
+        public JsonResult ProduceChart1Data()
+        {
+            var myData = new List<MorrisBarData>();
+            MorrisBarData data = null;
+            foreach (var priority in db.TicketPriorities.ToList())
+            {
+                data = new MorrisBarData();
+                data.label = priority.PriorityName;
+                data.value = db.Tickets.Where(t => t.TicketPriority.PriorityName == priority.PriorityName).Count();
+                myData.Add(data);
+            }
+            return Json(myData);
+        }
+
+        public JsonResult ProduceChart2Data()
+        {
+            var myData = new List<MorrisDonutData>();
+            MorrisDonutData data = null;
+            foreach (var priority in db.TicketPriorities.ToList())
+            {
+                data = new MorrisDonutData();
+                data.label = priority.PriorityName;
+                data.value = db.Tickets.Where(t => t.TicketPriority.PriorityName == priority.PriorityName).Count();
+                myData.Add(data);
+            }
+            return Json(myData);
+        }
+
+        public JsonResult ProduceChart3Data()
+        {
+            var myData = new List<MorrisBarData>();
+            MorrisBarData data = null;
+            foreach (var priority in db.TicketStatuses.ToList())
+            {
+                data = new MorrisBarData();
+                data.label = priority.StatusName;
+                data.value = db.Tickets.Where(t => t.TicketStatus.StatusName == priority.StatusName).Count();
+                myData.Add(data);
+            }
+            return Json(myData);
+        }
+
+        public JsonResult ProduceChart4Data()
+        {
+            var myData = new List<MorrisDonutData>();
+            MorrisDonutData data = null;
+            foreach (var priority in db.TicketStatuses.ToList())
+            {
+                data = new MorrisDonutData();
+                data.label = priority.StatusName;
+                data.value = db.Tickets.Where(t => t.TicketStatus.StatusName == priority.StatusName).Count();
+                myData.Add(data);
+            }
+            return Json(myData);
+        }
+
+        public JsonResult ProduceChart5Data()
+        {
+            var myData = new List<MorrisBarData>();
+            MorrisBarData data = null;
+            foreach (var priority in db.TicketTypes.ToList())
+            {
+                data = new MorrisBarData();
+                data.label = priority.TypeName;
+                data.value = db.Tickets.Where(t => t.TicketType.TypeName == priority.TypeName).Count();
+                myData.Add(data);
+            }
+            return Json(myData);
+        }
+
+        public JsonResult ProduceChart6Data()
+        {
+            var myData = new List<MorrisDonutData>();
+            MorrisDonutData data = null;
+            foreach (var priority in db.TicketTypes.ToList())
+            {
+                data = new MorrisDonutData();
+                data.label = priority.TypeName;
+                data.value = db.Tickets.Where(t => t.TicketType.TypeName == priority.TypeName).Count();
+                myData.Add(data);
+            }
+            return Json(myData);
+        }
+
+
+    }
+}
--- JF-BugTracker/Models/TicketPriority.cs
+++ JF-BugTracker/Models/TicketPriority.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 
 namespace JF_BugTracker.Models
 {
@@ -11,10 +8,10 @@ namespace JF_BugTracker.Models
         {
             Tickets = new HashSet<Ticket>();
         }
-        public int Id { get; set; }        
+        public int Id { get; set; }
         public string PriorityName { get; set; }
         public string Description { get; set; }
 
-        public virtual ICollection<Ticket>Tickets { get; set; }
+        public virtual ICollection<Ticket> Tickets { get; set; }
     }
 }
